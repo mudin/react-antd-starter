6b243d8458431656cc8e0e94d487820a169ef5e9
Mudin Ibrahim <mudin@3i.ai>
2021-02-16 09:03:31 +0900

    First React App setup with craco and antd supporting tree shaking

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import logo from './logo.svg';
-import './App.css';
+import { Button } from 'antd';
+import './App.less';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Button type='primary'>Button</Button>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
